eaec96c61ae717fae8ecb8d6276c320f88880b64
alozie2k <alozie2k@gmail.com>
2023-01-08 19:22:26 -0500

    Update SearchRepositoryImpl.java

--- Job Post/src/main/java/com/igbo/joblisting/repository/SearchRepositoryImpl.java
+++ Job Post/src/main/java/com/igbo/joblisting/repository/SearchRepositoryImpl.java
@@ -1,10 +1,10 @@
-package com.telusko.joblisting.repository;
+package com.igbi.JobPost.joblisting.repository;
 
 import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.telusko.joblisting.model.Post;
+import com.igbi.JobPost.model.Post;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
@@ -29,7 +29,7 @@ public class SearchRepositoryImpl implements SearchRepository{
 
         final List<Post> posts = new ArrayList<>();
 
-        MongoDatabase database = client.getDatabase("telusko");
+        MongoDatabase database = client.getDatabase("igbo");
         MongoCollection<Document> collection = database.getCollection("JobPost");
 
         AggregateIterable<Document> result = collection.aggregate(Arrays.asList(new Document("$search",
